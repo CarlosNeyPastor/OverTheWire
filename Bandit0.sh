24501e658fb62017c695c611e6cf24a42c76b484
CarlosNeyPastor <carlosneypastor@gmail.com>
2021-06-11 19:21:03 -0300

    Update Bandit0.sh

--- Bandit0.sh
+++ Bandit0.sh
@@ -4,6 +4,6 @@ HOST=bandit.labs.overthewire.org
 USUARIO=bandit0
 PASSWORD=bandit0
 PUERTO=2220
-SALIDA=Solucion.txt
+SALIDA=Solucion.Bandit1
 
 sshpass -p $PASSWORD ssh $USUARIO@$HOST -p $PUERTO  cat readme > $SALIDA
